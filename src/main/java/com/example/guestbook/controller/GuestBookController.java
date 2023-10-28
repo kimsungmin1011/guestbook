045c32516d5632af46d32a9798d54d85f9951e3a
kimsungmin1011 <tata153@naver.com>
2023-10-28 17:42:04 +0900

    jar 파일 추가

--- src/main/java/com/example/guestbook/controller/GuestBookController.java
+++ src/main/java/com/example/guestbook/controller/GuestBookController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController  // 이 클래스가 REST API의 컨트롤러 역할을 하는 클래스임을 나타냅니다.
 @RequestMapping("/guestbook")  // 이 컨트롤러에서 처리하는 모든 요청의 기본 URL 경로를 "/guestbook"으로 설정합니다.
-@CrossOrigin(origins = "http://localhost:3000") // React 앱의 URL을 여기에 지정
+@CrossOrigin(origins = "*") // React 앱의 URL을 여기에 지정
 public class GuestBookController {
 
     @Autowired  // GuestBookService 타입의 빈을 자동으로 주입받습니다.
